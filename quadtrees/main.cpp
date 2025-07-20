c518ccd00c12d0093a17c69a7b83eb346dab0344
Stanisław Grochowski <grochowski0stanislaw@gmail.com>
2025-07-20 11:44:35 +0200

    Barnes-Hut Algorithm full implementation with tree rebuild

--- quadtrees/main.cpp
+++ quadtrees/main.cpp
@@ -8,82 +8,69 @@
 #include "QuadTreeNode.h"
 #include "Particle.h"
 
-#define MIN 0.1
-/*
-void simulate(std::vector<Particle>& particles, QuadTreeNode& root, double dt) {
-    double scale = 1/pow(particles.size(),2);
-    
-    for (int i = 0; i < particles.size(); i++) {
-        Particle& particle = particles[i];
-        auto others = root.query(particle.position, 0.1);
-        Vector2d total_acceleration = { 0,0 };
-        for (Particle* other : others) {
-            if (other != &particle) {
-                Vector2d distance = particle.position.distance(other->position);
-                Vector2d unit_vector = distance.unit();
-                double distance_length = distance.length();
-                distance_length = std::min(distance_length, MIN);
-                total_acceleration += { scale * (1 / pow(distance_length, 2)) * unit_vector.x, scale * (1 / pow(distance_length, 2)) * unit_vector.y };
-            }
-        }
-        particle.updateAcceletation(total_acceleration);
+void simulate(std::vector<Particle>& particles, QuadTreeNode& root, const double dt) {
+    for (Particle& particle: particles) {
+        Vector2d force = root.getTotalForce(&particle);
+        particle.updateAcceletation(force);
     }
-    for (int i = 0; i < particles.size(); i++) {
-        particles[i].move(dt);
+    for (Particle& particle : particles) {
+        particle.move(dt);
     }
 }
-*/
-void updateVisualisation(std::vector<Particle>& particles, std::vector<sf::CircleShape>& particles_visual, const sf::Vector2u windowDim) {
+
+void updateVisualisation(std::vector<Particle>& particles, std::vector<sf::CircleShape>& particlesVisual, const sf::Vector2u windowDim, sf::RenderWindow& window) {
     for (int i = 0; i < particles.size(); i++) {
-        particles_visual[i].setPosition(sf::Vector2f(particles[i].position.x * windowDim.x, particles[i].position.y * windowDim.y));
+        particlesVisual[i].setPosition(sf::Vector2f(particles[i].position.x * windowDim.x, particles[i].position.y * windowDim.y));
+        window.draw(particlesVisual[i]);
+    }
+}
+
+void insertParticles(std::vector<Particle>& particles, QuadTreeNode& root) {
+    root.reset();
+    for (Particle& particle : particles) {
+        root.addParticle(&particle);
     }
 }
 
 int main()
 {
     srand(time(NULL));
-    const unsigned int particleCount = 25;
+    const unsigned int particleCount = 200;
 
     std::vector<Particle> particles;
     particles.reserve(particleCount);
     for (int i = 0; i < particleCount; i++) {
-        particles.emplace_back(Vector2d(double(rand()) / RAND_MAX, double(rand()) / RAND_MAX));
+        particles.emplace_back();
     }
+    
     QuadTreeNode root;
-    for (Particle& particle : particles) {
-        root.addParticle(&particle);
-    }
 
     const sf::Vector2u windowDimentions = { 600,600 };
     sf::RenderWindow window(sf::VideoMode(windowDimentions), "QuadTrees");
-    std::vector<sf::CircleShape> particles_visual;
-    for (int i = 0; i < particles.size(); ++i) {
-        sf::CircleShape particle(5.f);
-        particle.setPosition(sf::Vector2f( particles[i].position.x * windowDimentions.x, particles[i].position.y * windowDimentions.y));
-        particle.setFillColor(sf::Color(255, 255, 255));
-        particles_visual.push_back(particle);
+    
+    std::vector<sf::CircleShape> particlesVisual;
+    for (Particle& particle: particles) {
+        sf::CircleShape circle(particle.mass * 5.f);
+        circle.setPosition(sf::Vector2f(particle.position.x * windowDimentions.x, particle.position.y * windowDimentions.y));
+        circle.setFillColor(sf::Color(255, 255, 255));
+        particlesVisual.push_back(circle);
     }
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
     while (window.isOpen()) {
-        window.clear(sf::Color::Black);
-        root.draw(window);
-        updateVisualisation(particles, particles_visual, windowDimentions);
-        for (const auto& particle : particles_visual) {
-            window.draw(particle);
-        }
-        window.display();
-
-
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
         double time_elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000000.f;
         std::cout << "fps: " << 1 / time_elapsed << '\n';
         begin = end;
-        for (auto& particle : particles) {
-            particle.move(time_elapsed*0.01);
-        }
-        //simulate(particles, root, time_elapsed);
+
+        insertParticles(particles, root);
+        simulate(particles, root, time_elapsed * 0.01);
+
+        window.clear(sf::Color::Black);
+        root.draw(window);
+        updateVisualisation(particles, particlesVisual, windowDimentions, window);
+        window.display();
     }
 
     return 0;
